baac695882f989bacf8b0bdb329b5678ea568bcc
ARombakh <artromb@mail.ru>
2025-08-09 11:19:38 +0300

    Заменены obj на self

--- day1/date.py
+++ day1/date.py
@@ -5,32 +5,32 @@ class Date2025:
               7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
 
     # Инициализирует дату
-    def init(obj, day, month):
-        obj.day = day
-        obj.month = month
+    def __init__(self, day, month):
+        self.day = day
+        self.month = month
         Date2025.count += 1
 
     # Печатает дату
-    def print_dt(obj):
-        print(str(obj.day).zfill(2) + "-"
-              + str(obj.month).zfill(2) + "-"
-              + str(obj.year))
+    def __str__(self):
+        print(str(self.day).zfill(2) + "-"
+              + str(self.month).zfill(2) + "-"
+              + str(self.year))
 
     # Сравнивает даты
     def compare_bool(obj1, obj2):
         print(obj1.day == obj2.day and obj1.month == obj2.month)
 
     # Сравнивает даты
-    def compare_sign(obj1, obj2):
-        if obj1.month < obj2.month:
+    def compare_sign(self, obj2):
+        if self.month < obj2.month:
             print("Первая дата больше второй")    # Первая дата больше второй
-        elif(obj1.month == obj2.month and obj1.day < obj2.day):
+        elif(self.month == obj2.month and self.day < obj2.day):
             print("Первая дата больше второй")    # Первая дата больше второй
-        elif(obj1.month > obj2.month):
+        elif(self.month > obj2.month):
             print("Вторая дата больше первой")    # Вторая дата больше первой
-        elif(obj1.month == obj2.month and obj1.day > obj2.day):
+        elif(self.month == obj2.month and self.day > obj2.day):
             print("Вторая дата больше первой")    # Вторая дата больше первой
-        elif(obj1.day == obj2.day and obj1.month == obj2.month):
+        elif(self.day == obj2.day and self.month == obj2.month):
             print("Даты равны")
 
     # Добавляет к дате 1 день
@@ -45,16 +45,16 @@ class Date2025:
             obj.day += 1
 
 
-dt1 = Date2025()
-dt3 = Date2025()
-Date2025.init(dt1, 15, 3)
-Date2025.init(dt3, 7, 7)
-Date2025.print_dt(dt1)
+dt1 = Date2025(15, 3)
+dt3 = Date2025(7, 7)
+# Date2025.init(dt1, 15, 3)
+# Date2025.init(dt3, 7, 7)
+Date2025.__str__(dt1)
 
-dt2 = Date2025()
+dt2 = Date2025(31, 3)
 
-Date2025.init(dt2, 31, 3)
-Date2025.print_dt(dt2)
+# Date2025.init(dt2, 31, 3)
+Date2025.__str__(dt2)
 """
 print(dt2 is dt1)
 
@@ -64,11 +64,18 @@ print("2 объект день напрямую " + str(dt2.day))
 Date2025.compare_sign(dt1, dt2)
 
 print("dt2 before increment:")
-Date2025.print_dt(dt2)
+Date2025.__str__(dt2)
 
 Date2025.add_day(dt2)
+dt2.add_day()
 
 print("dt2 after increment:")
-Date2025.print_dt(dt2)
+Date2025.__str__(dt2)
 
-print(dt1.__dict__)
+
+print("print(dt2)")
+print(dt2.__str__())
+
+#print(object.__dict__)
+#print(object.__str__)
+
